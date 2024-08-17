ef413d5128b6e1d2bdf0277d3a85cbcaebe9fdd8
Florent SABBE <florent.sabbe@gmail.com>
2024-08-17 16:43:52 +0200

    Changes workflow

--- genetic/src/lib.rs
+++ genetic/src/lib.rs
@@ -1,12 +1,11 @@
 use std::fmt::Debug;
 
 pub mod evolution;
-pub mod selection;
 pub mod individual;
+pub mod selection;
 
 #[derive(Debug, Clone)]
 pub struct Evaluation<State> {
     pub state: State,
     pub fitness: f32,
 }
-
